0423162ccd0e8dff275ed2b9215065d2453b7d07
AJackTi <AJackTi@AJACKTI>
2020-03-12 18:47:03 +0700

    Add Project

---
+++ BookListRazor/BookListRazor/Controllers/BookController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Controllers
+{
+    [Route("api/Book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Books.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Books.FirstOrDefaultAsync(t => t.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            _db.Books.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
+        }
+    }
+}
---
+++ BookListRazor/BookListRazor/Pages/BookList/Upsert.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor
+{
+    public class UpsertModel : PageModel
+    {
+        private ApplicationDbContext _db;
+        public UpsertModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            Book = new Book();
+            if (id==null)
+            {
+                //create
+                return Page();
+            }
+
+            //update
+            Book = await _db.Books.FirstOrDefaultAsync(t=>t.Id==id);
+            if (Book==null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                if (Book.Id == 0)
+                {
+                    _db.Books.Add(Book);
+                }
+                else
+                {
+                    _db.Books.Update(Book);
+                }
+                //var BookFromDb = await _db.Books.FindAsync(Book.Id);
+                //BookFromDb.Name = Book.Name;
+                //BookFromDb.Author = Book.Author;
+                //BookFromDb.ISBN = Book.ISBN;
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+    }
+}
